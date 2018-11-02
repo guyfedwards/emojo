11d1d06a99469d5735f31dd5dd010c06f4b3d7d4
benswinburne <ben.swinburne@gmail.com>
2018-11-02 16:27:38 -0400

    Added eslint and prettier

--- src/handler.js
+++ src/handler.js
@@ -1,20 +1,21 @@
-const https = require('https')
+const https = require('https');
 const qs = require('querystring');
 
 const verify = (data, callback) => {
-  console.log('verify', process)
-  if (data.token === process.env.VERIFICATION_TOKEN) callback(null, data.challenge);
-  else callback("verification failed");
-}
+  console.log('verify', process);
+  if (data.token === process.env.VERIFICATION_TOKEN)
+    callback(null, data.challenge);
+  else callback('verification failed'); //eslint-disable-line
+};
 
 const handle = (event, callback) => {
   // test the message for a match and not a bot
-  if (!event.bot_id && /(emoji)/ig.test(event.text)) {
+  if (!event.bot_id && /(emoji)/gi.test(event.text)) {
     var text = `<@${event.user}> isn't AWS Lambda awesome?`;
     var message = {
       token: process.env.ACCESS_TOKEN,
       channel: event.channel,
-      text: text
+      text: text,
     };
 
     var query = qs.stringify(message); // prepare the querystring
@@ -22,17 +23,17 @@ const handle = (event, callback) => {
   }
 
   callback(null);
-}
+};
 
 exports.handler = (data, context, callback) => {
-  const { body } = data
-  const b = JSON.parse(body)
+  const { body } = data;
+  const b = JSON.parse(body);
   switch (b.type) {
-    case "url_verification": {
+    case 'url_verification': {
       verify(body, callback);
       break;
     }
-    case "event_callback": {
+    case 'event_callback': {
       handle(body.event, callback);
       break;
     }
