183ce53609949da28baeb07b6e245b5451368041
benswinburne <ben.swinburne@gmail.com>
2018-11-09 23:18:09 -0500

    test: add jest deps, config and commands

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  // Indicates whether each individual test should be reported during the run.
+  verbose: false,
+
+  noStackTrace: true,
+
+  // The directory where Jest should output its coverage files.
+  coverageDirectory: './.coverage/',
+
+  // The paths to modules that run some code to configure or set up the testing
+  // environment before each test.
+  setupFiles: ['<rootDir>/__tests__/setup.js'],
+};
