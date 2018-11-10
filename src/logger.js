8fb87613794fddab9fc2abf275e1e23d87592b28
benswinburne <ben.swinburne@gmail.com>
2018-11-09 21:34:21 -0500

    chore: adding controllable level to logger

--- src/logger.js
+++ src/logger.js
@@ -1,11 +1,17 @@
 const { createLogger, format, transports } = require('winston');
 
 const logger = createLogger({
+  exitOnError: false,
+  level: process.env.LOG_LEVEL || 'debug',
   format: format.combine(
     format.timestamp(),
     format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
   ),
-  transports: [new transports.Console()],
+  transports: [
+    new transports.Console({
+      handleExceptions: true,
+    }),
+  ],
 });
 
 module.exports = logger;
