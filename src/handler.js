ba61be690c892833eaee6bf2f2eb8a0f83046f56
benswinburne <ben.swinburne@gmail.com>
2018-11-20 11:03:47 -0500

    refactor: moved a bit more out of handler

--- src/handler.js
+++ src/handler.js
@@ -1,14 +1,13 @@
 const fs = require('fs');
 const os = require('os');
 const path = require('path');
-const AWS = require('aws-sdk');
 const axios = require('axios');
 const crypto = require('crypto');
 
 const logger = require('./logger');
-const { slack } = require('./slack');
 const verify = require('./url-verification');
 const { uploadToGithub } = require('./github');
+const { slack, sendPreview } = require('./slack');
 const { fileTypeIsSupported, streamAsPromise, getResizer } = require('./utils');
 
 const EMOJO_REGEX = /^:(\w+):$/;
@@ -27,33 +26,6 @@ const getCorrespondingEmojiMessageFromEvent = async event => {
   return (EMOJO_REGEX.exec(message.text) || []).pop();
 };
 
-// Now that we're checking the file upload has an alias, we could probably revert
-// this back to actually uploading a file to the channel rather than sending a
-// message with an attachment which would mean we didn't need to upload to s3.
-// I haven't removed it in the event that we're keeping all of them for a showcase
-// website which we discussed briefly.
-const sendPreviewToSlack = async (emojiAlias, metadata, tmpPath) => {
-  const s3upload = await uploadToS3(emojiAlias, metadata, tmpPath);
-
-  logger.info(`Uploaded to s3 ${s3upload.Location}`);
-
-  try {
-    const response = slack('chat.postMessage', {
-      channel: metadata.file.channels.join(','),
-      text: 'This is my attempt at the emoji you asked for',
-      attachments: JSON.stringify([
-        { fallback: s3upload.Location, image_url: s3upload.Location },
-      ]),
-    });
-
-    logger.info(`Sent to slack: ${s3upload.Location}`);
-
-    return response;
-  } catch (e) {
-    logger.error(`Failed to upload to slack: ${s3upload.Location}`, e);
-  }
-};
-
 const downloadImage = url => {
   return axios({
     method: 'GET',
@@ -63,25 +35,6 @@ const downloadImage = url => {
       Authorization: `Bearer ${process.env.ACCESS_TOKEN}`,
     },
   });
-};
-
-const uploadToS3 = async (key, metadata, tmpPath) => {
-  const s3 = new AWS.S3();
-
-  try {
-    return s3
-      .upload({
-        Bucket: process.env.S3_BUCKET,
-        Key: `emoji/key`,
-        // Body: writeStream,
-        Body: fs.createReadStream(tmpPath),
-        ContentType: metadata.file.mimetype,
-        ACL: 'public-read',
-      })
-      .promise();
-  } catch (e) {
-    logger.error(`Failed to upload to s3: ${tmpPath} %s`, e);
-  }
 };
 
 // const validate = message => {
@@ -141,10 +94,10 @@ const handle = async message => {
 
   // Both of these currently rely on the write to file happening which is why we
   // need to wait for that at the moment. What we can do is pass the stream into
-  // uploadToGithub and sendPreviewToSlack and that way we can just have them only
+  // uploadToGithub and sendPreview and that way we can just have them only
   // resolve when their respective stream has finished and remove the bit above
   await Promise.all([
-    sendPreviewToSlack(emojiAlias, metadata, tmpPath),
+    sendPreview(emojiAlias, metadata, tmpPath),
     uploadToGithub(emojiAlias, metadata, tmpPath),
   ]);
 
--- src/slack.js
+++ src/slack.js
@@ -1,7 +1,31 @@
+const fs = require('fs');
+const AWS = require('aws-sdk');
 const Slack = require('slack-node');
+
+const logger = require('./logger');
+
 const instance = new Slack(process.env.ACCESS_TOKEN);
 
-exports.slack = (method, params) => {
+const uploadToS3 = async (key, metadata, tmpPath) => {
+  const s3 = new AWS.S3();
+
+  try {
+    return s3
+      .upload({
+        Bucket: process.env.S3_BUCKET,
+        Key: `emoji/key`,
+        // Body: writeStream,
+        Body: fs.createReadStream(tmpPath),
+        ContentType: metadata.file.mimetype,
+        ACL: 'public-read',
+      })
+      .promise();
+  } catch (e) {
+    logger.error(`Failed to upload to s3: ${tmpPath} %s`, e);
+  }
+};
+
+const slack = (method, params) => {
   return new Promise((resolve, reject) => {
     instance.api(method, params, (err, response) => {
       err || !response.ok
@@ -10,3 +34,32 @@ exports.slack = (method, params) => {
     });
   });
 };
+
+// Now that we're checking the file upload has an alias, we could probably revert
+// this back to actually uploading a file to the channel rather than sending a
+// message with an attachment which would mean we didn't need to upload to s3.
+// I haven't removed it in the event that we're keeping all of them for a showcase
+// website which we discussed briefly.
+exports.sendPreview = async (emojiAlias, metadata, tmpPath) => {
+  const s3upload = await uploadToS3(emojiAlias, metadata, tmpPath);
+
+  logger.info(`Uploaded to s3 ${s3upload.Location}`);
+
+  try {
+    const response = slack('chat.postMessage', {
+      channel: metadata.file.channels.join(','),
+      text: 'This is my attempt at the emoji you asked for',
+      attachments: JSON.stringify([
+        { fallback: s3upload.Location, image_url: s3upload.Location },
+      ]),
+    });
+
+    logger.info(`Sent to slack: ${s3upload.Location}`);
+
+    return response;
+  } catch (e) {
+    logger.error(`Failed to upload to slack: ${s3upload.Location}`, e);
+  }
+};
+
+exports.slack = slack;
